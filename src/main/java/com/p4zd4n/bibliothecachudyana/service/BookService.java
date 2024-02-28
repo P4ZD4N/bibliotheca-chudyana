c3d9125445cfa8d9aaa00afdc5dd0f2b84212366
Wiktor <wiko700@gmail.com>
2024-02-28 22:26:11 +0100

    Added getNewReleases() method

--- src/main/java/com/p4zd4n/bibliothecachudyana/service/BookService.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/service/BookService.java
@@ -1,7 +1,11 @@
 package com.p4zd4n.bibliothecachudyana.service;
 
+import com.p4zd4n.bibliothecachudyana.entity.Book;
+
+import java.util.List;
 import java.util.Map;
 
 public interface BookService {
     Map<String, Integer> getTopCategories();
+    List<Book> getNewReleases();
 }
