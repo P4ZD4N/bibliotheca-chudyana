d61489a3b1a0749b83b59b711b62af36cf0eae01
Wiktor <wiko700@gmail.com>
2024-03-01 20:10:49 +0100

    Added SecurityConfig in new 'config' package

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.p4zd4n.bibliothecachudyana.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+        )
+        .formLogin(login -> login
+                .loginPage("/login")
+                .loginProcessingUrl("/")
+                .permitAll()
+        )
+        .logout(logout -> logout
+                .logoutUrl("/logout")
+                .permitAll()
+        );
+
+        return http.build();
+    }
+}
