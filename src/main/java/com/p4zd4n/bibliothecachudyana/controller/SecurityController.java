5dc3730d83600509a5ecbed42bc62d9d676461e3
Wiktor <wiko700@gmail.com>
2024-03-01 21:10:19 +0100

    Added password hashing to register() method

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/SecurityController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/SecurityController.java
@@ -4,6 +4,7 @@ import com.p4zd4n.bibliothecachudyana.dao.AuthorityDAO;
 import com.p4zd4n.bibliothecachudyana.dao.UserDAO;
 import com.p4zd4n.bibliothecachudyana.entity.Authority;
 import com.p4zd4n.bibliothecachudyana.entity.User;
+import com.p4zd4n.bibliothecachudyana.util.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,9 +35,14 @@ public class SecurityController {
 
     @PostMapping("/register")
     public String register(@ModelAttribute("user") User user) {
+        String hashedPassword = PasswordEncoder.encodePassword(user.getPassword());
+
         user.setEnabled(1);
+        user.setPassword("{bcrypt}" + hashedPassword);
+
         userDAO.save(user);
         authorityDAO.save(new Authority(user.getUsername(), "ROLE_USER"));
+
         return "redirect:/";
     }
 }
