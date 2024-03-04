f42ed68b61e9c094616d705ced2ddc492d2545e5
Wiktor <wiko700@gmail.com>
2024-03-04 22:53:41 +0100

    Added HomeController

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.p4zd4n.bibliothecachudyana.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+}
