d8b24e6376e2cbdeac185b93dee2b3ed05bd658a
Wiktor <wiko700@gmail.com>
2024-03-04 22:53:57 +0100

    Added NewReleasesController

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/NewReleasesController.java
@@ -0,0 +1,24 @@
+package com.p4zd4n.bibliothecachudyana.controller;
+
+import com.p4zd4n.bibliothecachudyana.entity.Book;
+import com.p4zd4n.bibliothecachudyana.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class NewReleasesController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/new-releases")
+    public String showNewReleases(Model model) {
+        List<Book> newBooks = bookService.getNewReleases();
+        model.addAttribute("newBooks", newBooks);
+        return "/newreleases/new-releases";
+    }
+}
