7c50a1bcab7d2e1e4fbb6de5716dc488a5869cf0
Wiktor <wiko700@gmail.com>
2024-03-05 22:48:44 +0100

    Added method getUserAuthorities()

--- src/main/java/com/p4zd4n/bibliothecachudyana/dao/AuthorityDAO.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/dao/AuthorityDAO.java
@@ -2,6 +2,9 @@ package com.p4zd4n.bibliothecachudyana.dao;
 
 import com.p4zd4n.bibliothecachudyana.entity.Authority;
 
+import java.util.List;
+
 public interface AuthorityDAO {
     void save(Authority authority);
+    List<String> getUserAuthorities(String username);
 }
