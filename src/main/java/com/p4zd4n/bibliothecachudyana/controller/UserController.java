a56c4a61707d6d894427aa617c820635a17ab400
Wiktor <wiko700@gmail.com>
2024-03-05 22:50:50 +0100

    Updated method displayUserDetails(), since now on endpoint /user/**username** we can see roles of user which we entered

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/UserController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.p4zd4n.bibliothecachudyana.controller;
 
+import com.p4zd4n.bibliothecachudyana.dao.AuthorityDAO;
 import com.p4zd4n.bibliothecachudyana.dao.UserDAO;
 import com.p4zd4n.bibliothecachudyana.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class UserController {
 
     @Autowired
     private UserDAO userDAO;
 
+    @Autowired
+    private AuthorityDAO authorityDAO;
+
     @GetMapping("/user/{username}")
     public String displayUserDetails(@PathVariable String username, Model model) {
         User user = userDAO.findByUsername(username);
+        List<String> authorities = authorityDAO.getUserAuthorities(username);
+
         model.addAttribute("user", user);
+        if (authorities.size() > 0)
+            model.addAttribute("authorities", authorities);
+
         return "/user/user";
     }
 }
