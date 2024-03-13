0a20fb6fa40a1fb7d1c6395f241c735913815ae2
Wiktor <wiko700@gmail.com>
2024-03-13 22:09:28 +0100

    Removed getUserAuthorities method implementation

--- src/main/java/com/p4zd4n/bibliothecachudyana/dao/implementation/AuthorityDAOImpl.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/dao/implementation/AuthorityDAOImpl.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Repository
 public class AuthorityDAOImpl implements AuthorityDAO {
 
@@ -23,12 +21,5 @@ public class AuthorityDAOImpl implements AuthorityDAO {
     @Override
     public void save(Authority authority) {
         entityManager.persist(authority);
-    }
-
-    @Override
-    public List<String> getUserAuthorities(String username) {
-        return entityManager.createQuery("SELECT authority.authority FROM Authority authority WHERE authority.username LIKE :username")
-                .setParameter("username", username)
-                .getResultList();
     }
 }
