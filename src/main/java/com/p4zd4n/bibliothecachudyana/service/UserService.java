d16610137b2fc72253773e7e65efe52f012b9ee8
Wiktor <wiko700@gmail.com>
2024-03-14 21:42:25 +0100

    Added addBookToWishlist method

--- src/main/java/com/p4zd4n/bibliothecachudyana/service/UserService.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/service/UserService.java
@@ -1,8 +1,10 @@
 package com.p4zd4n.bibliothecachudyana.service;
 
+import com.p4zd4n.bibliothecachudyana.entity.Book;
 import com.p4zd4n.bibliothecachudyana.entity.User;
 
 public interface UserService {
     boolean isUserAlreadyRegistered(User user);
     void registerUser(User user);
+    void addBookToWishlist(User user, Book book);
 }
