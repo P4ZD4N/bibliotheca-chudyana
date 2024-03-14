f7cc0d9fefffab22a2d5f7913ea295bd7b5a431a
Wiktor <wiko700@gmail.com>
2024-03-14 23:05:47 +0100

    Added POST mapping on /remove-from-wishlist endpoint

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/UserController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/UserController.java
@@ -3,9 +3,7 @@ package com.p4zd4n.bibliothecachudyana.controller;
 import com.p4zd4n.bibliothecachudyana.dao.AuthorityDAO;
 import com.p4zd4n.bibliothecachudyana.dao.BookDAO;
 import com.p4zd4n.bibliothecachudyana.dao.UserDAO;
-import com.p4zd4n.bibliothecachudyana.entity.Authority;
-import com.p4zd4n.bibliothecachudyana.entity.Book;
-import com.p4zd4n.bibliothecachudyana.entity.User;
+import com.p4zd4n.bibliothecachudyana.entity.*;
 import com.p4zd4n.bibliothecachudyana.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -51,7 +49,11 @@ public class UserController {
     @GetMapping("/user/{username}/wishlist")
     public String displayUserWishlist(@PathVariable String username, Model model) {
         User user = userDAO.findByUsername(username);
+        List<WishlistItem> userWishlist = user.getWishlist().getItems();
+
         model.addAttribute("user", user);
+        model.addAttribute("userWishlist", userWishlist);
+
         return "/user/wishlist";
     }
 
@@ -65,6 +67,18 @@ public class UserController {
         userService.addBookToWishlist(user, book);
 
         userDAO.save(user);
+
+        return "redirect:/user/" + username;
+    }
+
+    @PostMapping("/remove-from-wishlist")
+    public String removeFromWishlist(@RequestParam("bookId") Integer id, Authentication authentication) {
+        String username = authentication.getName();
+
+        User user = userDAO.findByUsername(username);
+        Book book = bookDAO.findById(id);
+
+        userService.removeBookFromWishlist(user, book);
 
         return "redirect:/user/" + username;
     }
