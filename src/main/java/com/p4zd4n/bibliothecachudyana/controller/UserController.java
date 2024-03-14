5b5d0fc9681719bd2afdf509485d6da7c97707f1
Wiktor <wiko700@gmail.com>
2024-03-14 21:40:35 +0100

    Added DAO and Service fields, added POST mapping on /add-to-wishlist endpoint

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/UserController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.p4zd4n.bibliothecachudyana.controller;
 
 import com.p4zd4n.bibliothecachudyana.dao.AuthorityDAO;
+import com.p4zd4n.bibliothecachudyana.dao.BookDAO;
 import com.p4zd4n.bibliothecachudyana.dao.UserDAO;
 import com.p4zd4n.bibliothecachudyana.entity.Authority;
+import com.p4zd4n.bibliothecachudyana.entity.Book;
 import com.p4zd4n.bibliothecachudyana.entity.User;
+import com.p4zd4n.bibliothecachudyana.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,6 +26,12 @@ public class UserController {
 
     @Autowired
     private AuthorityDAO authorityDAO;
+
+    @Autowired
+    private BookDAO bookDAO;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/user/{username}")
     public String displayUserDetails(@PathVariable String username, Model model) {
@@ -41,5 +53,19 @@ public class UserController {
         User user = userDAO.findByUsername(username);
         model.addAttribute("user", user);
         return "/user/wishlist";
+    }
+
+    @PostMapping("/add-to-wishlist")
+    public String addToWishlist(@RequestParam("bookId") Integer id, Authentication authentication) {
+        String username = authentication.getName();
+
+        User user = userDAO.findByUsername(username);
+        Book book = bookDAO.findById(id);
+
+        userService.addBookToWishlist(user, book);
+
+        userDAO.save(user);
+
+        return "redirect:/user/" + username;
     }
 }
