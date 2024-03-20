bec0ffa7789eaa8db7796088820b75e953a88059
Wiktor <wiko700@gmail.com>
2024-03-20 22:04:10 +0100

    Added Cart entity

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/entity/Cart.java
@@ -0,0 +1,56 @@
+package com.p4zd4n.bibliothecachudyana.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> items;
+
+    public Cart() {}
+
+    public Cart(User user) {
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+
+    public void addBook(CartItem item) {
+        this.getItems().add(item);
+    }
+}
