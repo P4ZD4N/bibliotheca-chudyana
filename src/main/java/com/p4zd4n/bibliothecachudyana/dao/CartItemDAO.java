50897e9fc4271df5e06e6b683a14728198843053
Wiktor <wiko700@gmail.com>
2024-03-20 22:25:39 +0100

    Added CartItemDAO with implementation

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/dao/CartItemDAO.java
@@ -0,0 +1,8 @@
+package com.p4zd4n.bibliothecachudyana.dao;
+
+import com.p4zd4n.bibliothecachudyana.entity.Book;
+import com.p4zd4n.bibliothecachudyana.entity.User;
+
+public interface CartItemDAO {
+    void deleteBookFromCart(User user, Book book);
+}
---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/dao/implementation/CartItemDAOImpl.java
@@ -0,0 +1,34 @@
+package com.p4zd4n.bibliothecachudyana.dao.implementation;
+
+import com.p4zd4n.bibliothecachudyana.dao.CartItemDAO;
+import com.p4zd4n.bibliothecachudyana.entity.Book;
+import com.p4zd4n.bibliothecachudyana.entity.CartItem;
+import com.p4zd4n.bibliothecachudyana.entity.User;
+import com.p4zd4n.bibliothecachudyana.entity.WishlistItem;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class CartItemDAOImpl implements CartItemDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public CartItemDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    @Override
+    public void deleteBookFromCart(User user, Book book) {
+        CartItem cartItem = entityManager.createQuery(
+                        "SELECT c FROM CartItem c WHERE c.book.id = :bookId AND c.cart.user.id = :userId", CartItem.class
+                )
+                .setParameter("bookId", book.getId())
+                .setParameter("userId", user.getId())
+                .getSingleResult();
+        entityManager.remove(cartItem);
+    }
+}
