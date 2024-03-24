0779ff7a7e1b3c409b6b78e42199e5c2e4331fd3
Wiktor <wiko700@gmail.com>
2024-03-25 00:03:19 +0100

    Added OrderItem entity

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/entity/OrderItem.java
@@ -0,0 +1,52 @@
+package com.p4zd4n.bibliothecachudyana.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    public OrderItem() {}
+
+    public OrderItem(Order order, Book book) {
+        this.order = order;
+        this.book = book;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
