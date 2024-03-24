33537383b9c77923c942d4f895182252d121fda2
Wiktor <wiko700@gmail.com>
2024-03-25 00:02:57 +0100

    Added Order entity

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/entity/Order.java
@@ -0,0 +1,105 @@
+package com.p4zd4n.bibliothecachudyana.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "order_date")
+    private LocalDate orderDate;
+
+    @Column(name = "total_amount")
+    private Double totalAmount;
+
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+
+    @Column(name = "status")
+    private String status;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
+
+    public Order() {}
+
+    public Order(User user, LocalDate orderDate, Double totalAmount, String shippingAddress, String status) {
+        this.user = user;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+        this.shippingAddress = shippingAddress;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public void addBook(OrderItem item) {
+        this.getItems().add(item);
+    }
+}
