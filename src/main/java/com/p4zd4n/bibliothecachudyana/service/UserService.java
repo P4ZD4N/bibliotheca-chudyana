bacb8f89eafc6f878ee01588c363e0b708f2aeb6
Wiktor <wiko700@gmail.com>
2024-03-25 12:46:30 +0100

    Added and implemented DAO methods to improve clean architecture

--- src/main/java/com/p4zd4n/bibliothecachudyana/service/UserService.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/service/UserService.java
@@ -3,7 +3,14 @@ package com.p4zd4n.bibliothecachudyana.service;
 import com.p4zd4n.bibliothecachudyana.entity.Book;
 import com.p4zd4n.bibliothecachudyana.entity.User;
 
+import java.util.List;
+
 public interface UserService {
+    User findByUsername(String username);
+    List<User> findAll();
+    void save(User user);
+    void update(User user);
+    void delete(User user);
     boolean isUserAlreadyRegistered(User user);
     void registerUser(User user);
     void addBookToWishlist(User user, Book book);
--- src/main/java/com/p4zd4n/bibliothecachudyana/service/implementation/UserServiceImpl.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/service/implementation/UserServiceImpl.java
@@ -33,6 +33,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public User findByUsername(String username) {
+        return userDAO.findByUsername(username);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDAO.findAll();
+    }
+
+    @Override
+    public void save(User user) {
+        userDAO.save(user);
+    }
+
+    @Override
+    public void update(User user) {
+        userDAO.update(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        userDAO.delete(user);
+    }
+
+    @Override
     public boolean isUserAlreadyRegistered(User user) {
         User existingUser = userDAO.findByUsername(user.getUsername());
         return existingUser != null;
