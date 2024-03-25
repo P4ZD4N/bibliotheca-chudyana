8c850274a0572ad8da422f2a7ee40df9696bdaaa
Wiktor <wiko700@gmail.com>
2024-03-25 12:51:28 +0100

    Replaced all DAO methods with Service method, deleted DAO fields and cleaned up imports

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/SecurityController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/SecurityController.java
@@ -1,11 +1,7 @@
 package com.p4zd4n.bibliothecachudyana.controller;
 
-import com.p4zd4n.bibliothecachudyana.dao.AuthorityDAO;
-import com.p4zd4n.bibliothecachudyana.dao.BookDAO;
-import com.p4zd4n.bibliothecachudyana.dao.UserDAO;
 import com.p4zd4n.bibliothecachudyana.entity.*;
 import com.p4zd4n.bibliothecachudyana.service.UserService;
-import com.p4zd4n.bibliothecachudyana.util.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-
 @Controller
 public class SecurityController {
-
-    @Autowired
-    private UserDAO userDAO;
-
-    @Autowired
-    private AuthorityDAO authorityDAO;
-
-    @Autowired
-    private BookDAO bookDAO;
 
     @Autowired
     private UserService userService;
