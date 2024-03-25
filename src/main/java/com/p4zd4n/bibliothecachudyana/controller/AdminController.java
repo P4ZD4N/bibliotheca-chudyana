91e9dafbe1e950a0402841f341f7497e9c14e14e
Wiktor <wiko700@gmail.com>
2024-03-25 22:17:46 +0100

    Added AdminController

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.p4zd4n.bibliothecachudyana.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String displayAdminPanel() {
+        return "/admin/admin";
+    }
+}
