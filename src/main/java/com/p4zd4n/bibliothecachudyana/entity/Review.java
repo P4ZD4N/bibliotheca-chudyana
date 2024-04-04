259ef81c69b34bb2ab965c6b450f2ca77775b721
Wiktor <wiko700@gmail.com>
2024-04-04 11:17:59 +0200

    Added Review entity

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/entity/Review.java
@@ -0,0 +1,88 @@
+package com.p4zd4n.bibliothecachudyana.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "date_added")
+    private LocalDate dateAdded;
+
+    public Review() {}
+
+    public Review(Integer rating, String content, LocalDate dateAdded) {
+        this.rating = rating;
+        this.content = content;
+        this.dateAdded = dateAdded;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+}
