d49b02450b29f1ef3f87e7108fbeb0d17f27a373
Wiktor <wiko700@gmail.com>
2024-04-04 21:32:31 +0200

    Added 7 new findBy... methods and update(), delete() methods

--- src/main/java/com/p4zd4n/bibliothecachudyana/service/ReviewService.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/service/ReviewService.java
@@ -3,10 +3,20 @@ package com.p4zd4n.bibliothecachudyana.service;
 import com.p4zd4n.bibliothecachudyana.entity.Book;
 import com.p4zd4n.bibliothecachudyana.entity.Review;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ReviewService {
+    Review findById(Integer id);
     List<Review> findAll();
     List<Review> findByBook(Book book);
+    List<Review> findByUsername(String username);
+    List<Review> findByBookId(Integer bookId);
+    List<Review> findByRating(Integer rating);
+    List<Review> findByMinAddedDate(LocalDate minAddedDate);
+    List<Review> findByMaxAddedDate(LocalDate maxAddedDate);
+    List<Review> findByMinAndMaxAddedDate(LocalDate minAddedDate, LocalDate maxAddedDate);
     void save(Review review);
+    void update(Review review);
+    void delete(Review review);
 }
--- src/main/java/com/p4zd4n/bibliothecachudyana/service/implementation/ReviewServiceImpl.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/service/implementation/ReviewServiceImpl.java
@@ -2,11 +2,14 @@ package com.p4zd4n.bibliothecachudyana.service.implementation;
 
 import com.p4zd4n.bibliothecachudyana.dao.ReviewDAO;
 import com.p4zd4n.bibliothecachudyana.entity.Book;
+import com.p4zd4n.bibliothecachudyana.entity.Order;
 import com.p4zd4n.bibliothecachudyana.entity.Review;
 import com.p4zd4n.bibliothecachudyana.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,6 +17,11 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Autowired
     private ReviewDAO reviewDAO;
+
+    @Override
+    public Review findById(Integer id) {
+        return reviewDAO.findById(id);
+    }
 
     @Override
     public List<Review> findAll() {
@@ -26,7 +34,67 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public void save(Review review) {
+    public List<Review> findByUsername(String username) {
+        return reviewDAO.findByUsername(username);
+    }
+
+    @Override
+    public List<Review> findByBookId(Integer bookId) {
+        return reviewDAO.findByBookId(bookId);
+    }
+
+    @Override
+    public List<Review> findByRating(Integer rating) {
+        return reviewDAO.findByRating(rating);
+    }
+
+    @Override
+    public List<Review> findByMinAddedDate(LocalDate minAddedDate) {
+        List<Review> reviews = reviewDAO.findAll();
+        List<Review> reviewsAddedAfterMinDate = new ArrayList<>();
+
+        for (Review review : reviews)
+            if (review.getDateAdded().isAfter(minAddedDate) || review.getDateAdded().isEqual(minAddedDate))
+                reviewsAddedAfterMinDate.add(review);
+
+        return reviewsAddedAfterMinDate;
+    }
+
+    @Override
+    public List<Review> findByMaxAddedDate(LocalDate maxAddedDate) {
+        List<Review> reviews = reviewDAO.findAll();
+        List<Review> reviewsAddedBeforeMaxDate = new ArrayList<>();
+
+        for (Review review : reviews)
+            if (review.getDateAdded().isBefore(maxAddedDate) || review.getDateAdded().isEqual(maxAddedDate))
+                reviewsAddedBeforeMaxDate.add(review);
+
+        return reviewsAddedBeforeMaxDate;
+    }
+
+    @Override
+    public List<Review> findByMinAndMaxAddedDate(LocalDate minAddedDate, LocalDate maxAddedDate) {
+        List<Review> reviewsAddedAfterMinDate = findByMinAddedDate(minAddedDate);
+        List<Review> reviewsAddedBeforeMaxDate = findByMaxAddedDate(maxAddedDate);
+
+        List<Review> reviewsAddedAfterMinDateAndBeforeMaxDate = reviewsAddedAfterMinDate;
+        reviewsAddedAfterMinDateAndBeforeMaxDate.retainAll(reviewsAddedBeforeMaxDate);
+
+        return reviewsAddedAfterMinDateAndBeforeMaxDate;
+    }
+
+    @Override
+    public void save(Review review)  {
         reviewDAO.save(review);
+    }
+
+    @Override
+    public void update(Review review) {
+        reviewDAO.update(review);
+    }
+
+    @Override
+    public void delete(Review review) {
+        reviewDAO.delete(review);
     }
 }
