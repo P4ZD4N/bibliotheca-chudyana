01eeb05761890d1f2926ec033d45ec935e1827b4
Wiktor <wiko700@gmail.com>
2024-04-18 00:24:29 +0200

    Added CustomErrorController

---
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/CustomErrorController.java
@@ -0,0 +1,36 @@
+package com.p4zd4n.bibliothecachudyana.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            switch (statusCode) {
+
+                case 403 -> {
+                    return "/error/error403";
+                }
+                case 404 -> {
+                    return "/error/error404";
+                }
+                default -> {
+                    return "/error/error";
+                }
+            }
+        }
+
+        return "/error/error";
+    }
+}
