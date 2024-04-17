2ffa4ccc5ec27f0a1c67fb0a304a39a8878609f8
Wiktor <wiko700@gmail.com>
2024-04-18 00:26:33 +0200

    Added unathorized access protection

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/CartController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/CartController.java
@@ -31,7 +31,9 @@ public class CartController {
         boolean isAuthenticatedUserUnauthorizedToEnter = userService.isUnauthorizedToEnter(username, authenticatedUsername);
 
         if (isAuthenticatedUserUnauthorizedToEnter) {
-            return "redirect:/user/" + authenticatedUsername;
+            model.addAttribute("user", userService.findByUsername(authenticatedUsername));
+            model.addAttribute("error", "Brak dostępu!");
+            return "/user/user";
         }
 
         User user = userService.findByUsername(username);
