40cc4ff6d9cae199e0f202f41dadf6a77a4682c4
Wiktor <wiko700@gmail.com>
2024-04-18 00:28:29 +0200

    Added validation annotations to fields related with address

--- src/main/java/com/p4zd4n/bibliothecachudyana/entity/Order.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/entity/Order.java
@@ -1,6 +1,7 @@
 package com.p4zd4n.bibliothecachudyana.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -25,15 +26,22 @@ public class Order {
     private Double totalAmount;
 
     @Column(name = "house_number")
+    @NotNull(message = "Pole 'Numer domu' jest wymagane!")
+    @Digits(integer = 3, fraction = 0, message = "Pole 'Numer domu' musi być liczbą całkowitą!")
+    @Positive(message = "Pole 'Numer domu' musi być liczbą dodatnią!")
     private Integer houseNumber;
 
     @Column(name = "street")
+    @NotBlank(message = "Pole 'Ulica' jest wymagane!")
     private String street;
 
     @Column(name = "city")
+    @NotBlank(message = "Pole 'Miasto' jest wymagane!")
     private String city;
 
     @Column(name = "postal_code")
+    @NotBlank(message = "Pole 'Kod pocztowy' jest wymagane!")
+    @Pattern(regexp = "^\\d{2}-\\d{3}$", message = "Pole 'Kod pocztowy' musi być w formacie XX-XXX!")
     private String postalCode;
 
     @Column(name = "status")
