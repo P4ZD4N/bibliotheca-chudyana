e668b8e2a3b50114803343c6cc186c3c343c6a9a
Wiktor <wiko700@gmail.com>
2024-04-18 12:46:32 +0200

    Removed startDate and endDate fields and all what was related to them

--- src/main/java/com/p4zd4n/bibliothecachudyana/entity/Discount.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/entity/Discount.java
@@ -1,10 +1,7 @@
 package com.p4zd4n.bibliothecachudyana.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
-
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "discounts")
@@ -23,21 +20,11 @@ public class Discount {
     @NotNull(message = "Pole 'Zniżka' jest wymagane!")
     private Double discountPercentage;
 
-    @Column(name = "start_date")
-    @NotNull(message = "Pole 'Start' jest wymagane!")
-    private LocalDate startDate;
-
-    @Column(name = "end_date")
-    @NotNull(message = "Pole 'Koniec' jest wymagane!")
-    private LocalDate endDate;
-
     public Discount() {}
 
-    public Discount(Book book, Double discountPercentage, LocalDate startDate, LocalDate endDate) {
+    public Discount(Book book, Double discountPercentage) {
         this.book = book;
         this.discountPercentage = discountPercentage;
-        this.startDate = startDate;
-        this.endDate = endDate;
     }
 
     public Integer getId() {
@@ -62,26 +49,5 @@ public class Discount {
 
     public void setDiscountPercentage(Double discountPercentage) {
         this.discountPercentage = discountPercentage;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    @AssertTrue(message = "Data zakończenia musi być po dacie rozpoczęcia!")
-    private boolean isEndDateValid() {
-        return endDate == null || startDate == null || endDate.isAfter(startDate) || endDate.isEqual(startDate);
     }
 }
