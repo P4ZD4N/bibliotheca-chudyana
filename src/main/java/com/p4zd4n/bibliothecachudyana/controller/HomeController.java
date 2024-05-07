17938351088883ceb6d908397e8a0df98ad508d7
Wiktor <wiko700@gmail.com>
2024-05-07 20:09:59 +0200

    Renamed showHomePage() method to displayHomePage()

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/HomeController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String showHomePage() {
+    public String displayHomePage() {
         return "index";
     }
 }
