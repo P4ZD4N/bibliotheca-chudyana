38663c32e282225736b28a64bfb605b16dba6d8b
Wiktor <wiko700@gmail.com>
2024-05-09 00:36:52 +0200

    Changed @PostMapping to @GetMapping above removeFromCart() method

--- src/main/java/com/p4zd4n/bibliothecachudyana/controller/CartController.java
+++ src/main/java/com/p4zd4n/bibliothecachudyana/controller/CartController.java
@@ -59,7 +59,7 @@ public class CartController {
         return "redirect:/user/" + username + "/cart";
     }
 
-    @PostMapping("/remove-from-cart")
+    @GetMapping("/remove-from-cart")
     public String removeFromCart(@RequestParam("bookId") Integer id, Authentication authentication) {
         String username = authentication.getName();
 
